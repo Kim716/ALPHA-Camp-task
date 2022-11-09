7fc7923a55e1d7d8f453cc300c99194d18a3dce7
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-09 16:37:05 +0800

    week2 end

--- s2-2 | AC_Movie2/script.js
+++ s2-2 | AC_Movie2/script.js
@@ -4,10 +4,14 @@ const MOVIE_URL = BASE_URL + "/api/v1/movies/";
 const POSTER_URL = BASE_URL + "/posters/";
 const moviePanel = document.querySelector("#movie-panel");
 const searchForm = document.querySelector("#search-form");
+const paginator = document.querySelector(".pagination");
+// 分頁設計，決定每頁要有幾個
+const moviePerPage = 12;
 
 const movies = [];
+let filteredMovies = [];
 
-// FUNCTION render movie data
+////////////  FUNCTION render movie data
 function renderDatas(datas) {
   let rawHTML = "";
   for (const movie of datas) {
@@ -40,7 +44,7 @@ function renderDatas(datas) {
   moviePanel.innerHTML = rawHTML;
 }
 
-// FUNCTION show modal
+//////////// FUNCTION show modal
 function showModal(data) {
   const title = document.querySelector("#movie-modal-title");
   const img = document.querySelector("#movie-modal-img");
@@ -53,13 +57,11 @@ function showModal(data) {
   description.textContent = data.description;
 }
 
-// FUNCTION 搜尋電影
+//////////// FUNCTION 搜尋電影
 function searchMovies(keywords) {
-  let filteredMovies = [];
-
-  if (!keywords.length) {
-    return alert("Type something valid");
-  }
+  // if (!keywords.length) {
+  //   return alert("Type something valid");
+  // }
 
   // array.filter 會找出「所有」符合條件判斷式的陣列
   filteredMovies = movies.filter((movie) =>
@@ -76,10 +78,10 @@ function searchMovies(keywords) {
     return alert("can't find the movie");
   }
 
-  renderDatas(filteredMovies);
+  renderDatas(getMoviesByPage(1));
 }
 
-// FUNCTION 加入收藏清單
+//////////// FUNCTION 加入收藏清單
 function addFavoriteMovie(id) {
   // 創造一個放收藏電影的陣列清單，已經有一部以上就會取原本的，是null的話就建立空陣列
   const list = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
@@ -97,7 +99,31 @@ function addFavoriteMovie(id) {
   localStorage.setItem("favoriteMovies", JSON.stringify(list));
 }
 
-// EVENT LISTENER show modal
+//////////// FUNCTION render 分頁器
+function renderPaginator(amounts) {
+  // 算出要有幾頁
+  const numberOfPages = Math.ceil(amounts / moviePerPage);
+
+  let rawHTML = ``;
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`;
+  }
+
+  paginator.innerHTML = rawHTML;
+}
+
+//////////// FUNCTION 找出每頁要呈現出幾部電影
+function getMoviesByPage(page) {
+  // p1 -- index 0 ~ 11
+  // p2 -- index 12 ~ 23
+  // p3 -- index 24 ~ 35 ..
+  // 確認有沒有啟動搜尋功能，決定分頁要呈現的哪一個內容
+  const data = filteredMovies.length ? filteredMovies : movies;
+  const startIndex = moviePerPage * (page - 1);
+  return data.slice(startIndex, startIndex + moviePerPage);
+}
+
+//////////// EVENT LISTENER show modal
 // 點擊時跳出該電影資料
 moviePanel.addEventListener("click", function (event) {
   // 要在render時 moreBtn favBtn 加上 data-id="" 來辨識是哪部電影
@@ -114,7 +140,7 @@ moviePanel.addEventListener("click", function (event) {
   }
 });
 
-// EVENT LISTENER 產生搜尋結果
+//////////// EVENT LISTENER 產生搜尋結果
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault(); // 防止瀏覽器預設的跳轉頁面
   const keywords = document
@@ -123,9 +149,10 @@ searchForm.addEventListener("submit", (event) => {
     .toLowerCase();
 
   searchMovies(keywords);
+  renderPaginator(filteredMovies.length);
 });
 
-// EVENT LISTENER ：每按一次就搜尋一次的版本
+//////////// EVENT LISTENER ：每按一次就搜尋一次的版本
 /*
 searchForm.addEventListener("keyup", (event) => {
   event.preventDefault(); // 防止瀏覽器預設的跳轉頁面
@@ -145,11 +172,24 @@ searchForm.addEventListener("keyup", (event) => {
 });
 */
 
-// EXECUTE
+//////////// EVENT LISTENER 點擊分頁器，重新渲染畫面
+paginator.addEventListener("click", function (event) {
+  // 確認是否點擊到 a 標籤，
+  if (event.target.tagName !== "A") return;
+
+  // 看點擊到哪一頁，重新渲染
+  const page = Number(event.target.dataset.page);
+  renderDatas(getMoviesByPage(page));
+});
+
+///////////// EXECUTE
 axios
   .get(MOVIE_URL)
   .then((response) => {
     movies.push(...response.data.results);
-    renderDatas(movies);
+    // 分頁器長度
+    renderPaginator(movies.length);
+    // 根據頁數呈現資料
+    renderDatas(getMoviesByPage(1));
   })
   .catch((error) => console.log(error));
