ee7a599226303927b5bd6e7801cdacd06dd53428
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-21 19:35:01 +0800

    Matching Game U44 動態產生卡片元素

--- s2-2 | Matching Game/main.js
+++ s2-2 | Matching Game/main.js
@@ -1,1 +1,47 @@
 "use strict";
+// --- DATA --- //
+const Symbols = [
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png", // 黑桃
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png", // 愛心
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png", // 方塊
+  "https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png", // 梅花
+];
+
+// --- DOM nodes MVC 都會用到--- //
+const cardPanel = document.querySelector("#card-panel");
+
+const view = {
+  transformNumber: function (number) {
+    switch (number) {
+      case 1:
+        return "A";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        return number;
+    }
+  },
+
+  generateCardHTML: function (index) {
+    const number = this.transformNumber((index % 13) + 1);
+    const symbol = Symbols[Math.trunc(index / 13)];
+    return `<div class="card">
+        <p>${number}</p>
+        <img
+          src="${symbol}"
+          alt="花色"
+        />
+        <p>${number}</p>
+      </div>`;
+  },
+
+  renderCards: function () {
+    return (cardPanel.innerHTML = this.generateCardHTML(0));
+  },
+};
+
+view.renderCards();
