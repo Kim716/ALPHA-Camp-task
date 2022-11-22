10d2b9e37ff84e9212e89a464b269c5cd6210bcf
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-11-22 16:42:33 +0800

    Matching Game U47 點擊時翻牌（監聽牌桌）

--- s2-2 | Matching Game/main.js
+++ s2-2 | Matching Game/main.js
@@ -27,24 +27,56 @@ const view = {
     }
   },
 
-  generateCardHTML: function (index) {
+  getCardContent: function (index) {
     const number = this.transformNumber((index % 13) + 1);
     const symbol = Symbols[Math.trunc(index / 13)];
-    return `<div class="card">
+
+    if (index > 12 && index < 39) {
+      return `
+      <div class='for-card-EL'></div>
+        <p class='red'>${number}</p>
+        <img
+          src="${symbol}"
+          alt="花色"
+        />
+        <p class='red'>${number}</p>
+      `;
+    }
+    return `
+        <div class='for-card-EL'></div>
         <p>${number}</p>
         <img
           src="${symbol}"
           alt="花色"
         />
         <p>${number}</p>
-      </div>`;
+      `;
+  },
+
+  getCardElement: function (index) {
+    return `<div class="card back" data-index='${index}'></div>`;
   },
 
   renderCards: function () {
     cardPanel.innerHTML = utility
       .getRandomNumberArray(52)
-      .map((index) => this.generateCardHTML(index))
+      .map((index) => this.getCardElement(index))
       .join("");
+  },
+
+  flipCard: function (card) {
+    const cardIndex = Number(card.dataset.index);
+
+    // 背翻正
+    if (card.classList.contains("back")) {
+      card.classList.remove("back");
+      card.innerHTML = this.getCardContent(cardIndex);
+      return;
+    }
+
+    // 正翻背
+    card.classList.add("back");
+    card.innerHTML = "";
   },
 };
 
@@ -63,4 +95,28 @@ const utility = {
   },
 };
 
+// --- EVENT LISTENER --- //
+// EL-1 監聽翻牌事件（監聽牌桌）
+cardPanel.addEventListener("click", (e) => {
+  if (e.target.matches("#card-panel")) return;
+
+  if (e.target.matches(".card")) {
+    view.flipCard(e.target);
+    return;
+  }
+
+  if (e.target.matches(".for-card-EL")) {
+    view.flipCard(e.target.parentElement);
+  }
+});
+
+// --- EXECUTE --- //
 view.renderCards();
+
+// // EL-1 監聽翻牌事件（監聽每張牌）
+// document.querySelectorAll(".card").forEach((card) => {
+//   card.addEventListener("click", (e) => {
+//     console.log(e.target);
+//     view.flipCard(card);
+//   });
+// });
