14778a07ba76c268a6370f68f9caac58c910f8d8
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2022-12-07 16:01:00 +0800

    Initialize project:\ 使用 node.js, express.js 來建立初始環境

---
+++ S2-3_Movie_List_Express/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+app.listen(port, () => {
+  console.log("I'm listening");
+});
